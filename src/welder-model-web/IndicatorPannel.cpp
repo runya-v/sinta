9a5230cdd9152b49c8b6044901666cbc188d59c3
Rostislav Welichko <rostislav.vel@gmail.com>
2014-12-29 14:13:13 +0300

    Доработана панель индикации.

--- src/welder-model-web/IndicatorPannel.cpp
+++ src/welder-model-web/IndicatorPannel.cpp
@@ -20,14 +20,14 @@ IndicatorPannel::IndicatorPannel() {
     this->setStyleClass("pannel");
 
     Wt::WVBoxLayout *vbox = new Wt::WVBoxLayout();
-	vbox->setContentsMargins(5, 5, 5, 5);
+    vbox->setContentsMargins(5, 5, 5, 5);
     LcdIndicator *lcd_indicator = new LcdIndicator();
     vbox->addWidget(lcd_indicator);
     JobListPannel *job_list_pannel = new JobListPannel();
     vbox->addWidget(job_list_pannel);
 
     Wt::WHBoxLayout *hbox = new Wt::WHBoxLayout();
-	hbox->setContentsMargins(5, 5, 5, 5);
+    hbox->setContentsMargins(5, 5, 5, 5);
     hbox->addLayout(vbox, 1);
     WelderParamsPannel *welder_params_pannel = new WelderParamsPannel();
     hbox->addWidget(welder_params_pannel);
@@ -35,7 +35,7 @@ IndicatorPannel::IndicatorPannel() {
     hbox->addWidget(welding_types_pannel);
 
     Wt::WVBoxLayout *vlayout = new Wt::WVBoxLayout(this);
-	vlayout->setContentsMargins(5, 5, 5, 5);
+    vlayout->setContentsMargins(5, 5, 5, 5);
     vlayout->addWidget(new Wt::WText(Wt::WString::tr("IndicatorPannel")));
     vlayout->addLayout(hbox);
 }
