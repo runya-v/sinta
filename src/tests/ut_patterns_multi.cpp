7edc850819cd8c8b5722e302f264751ebdba555a
Rostislav Welichko <rostislav.vel@gmail.com>
2015-01-26 20:13:01 +0300

    Добавлена заготовка для тестов многопоточных паттернов.

---
+++ src/tests/ut_patterns_multi.cpp
@@ -0,0 +1,36 @@
+#ifndef BOOST_STATIC_LINK
+#   define BOOST_TEST_DYN_LINK
+#endif // BOOST_STATIC_LINK
+
+#define BOOST_TEST_MODULE PatternsMulti
+#define BOOST_AUTO_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+#include <boost/format.hpp>
+#include <boost/utility.hpp>
+
+
+class ActiveObject;
+class Balking;
+class MessagingDesignPattern;
+class GuardedSuspention;
+class HalfSyncHalfAsync;
+class LeadersFollowers;
+class Lock;
+class MonitorObject;
+class Reactor;
+class ReadWriteLock;
+class Sheduler;
+class ThreadPool;
+class ThreadSpecificStorage;
+class CooperativePattern;
+
+
+class PatternsMulti {
+
+};
+
+
+BOOST_AUTO_TEST_CASE(TestPatternsMulti) {
+}
